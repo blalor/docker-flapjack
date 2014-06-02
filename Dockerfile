2e5cc40088761075101eddbf5c0c055ea3476d23
Brian Lalor <blalor@bravo5.org>
2014-06-02 15:45:36 -0400

    Run UI and API behind port 80 (nginx)
    JSONAPI is at /api

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,7 @@
 FROM blalor/supervised:latest
 MAINTAINER Brian Lalor <blalor@bravo5.org>
 
-EXPOSE 3080
-EXPOSE 3081
-EXPOSE 3071
+EXPOSE 80
 
 ADD src /tmp/src/
 RUN /tmp/src/config.sh
--- src/config.sh
+++ src/config.sh
@@ -22,6 +22,12 @@ chown -R flapjack:flapjack /etc/flapjack /var/{run,log,lib}/flapjack
 yum install -y centos-release-SCL openssl-devel gcc-c++ patch collectd-curl_json
 yum install --disablerepo=* --enablerepo=scl -y ruby193-ruby-devel
 
+yum localinstall -y http://nginx.org/packages/centos/6/noarch/RPMS/nginx-release-centos-6-0.el6.ngx.noarch.rpm
+yum install -y nginx
+
+mv nginx.conf /etc/nginx/
+mv program-nginx.conf /etc/supervisor.d/
+
 ## gotta be a better way
 export LD_LIBRARY_PATH="$( scl enable ruby193 'echo ${LD_LIBRARY_PATH}' )"
 export PATH="$( scl enable ruby193 'echo ${PATH}' )"
